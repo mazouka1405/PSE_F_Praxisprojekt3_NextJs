812ddb2350aadc46b93c65ce45ed393d26c20fa3
mazouka1405 <mariamazouka@gmail.com>
2026-05-15 20:17:47 +0200

    Kapitel 7 abgeschlossen

---
+++ .next/static/chunks/cfecd_Frontend_Praxisprojekt3_NextJs_nextjs-dashboard_app_dashboard_page_tsx_1d000536._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/4409a_next_dist_6d0d040f._.js"
+],
+    source: "dynamic"
+});
