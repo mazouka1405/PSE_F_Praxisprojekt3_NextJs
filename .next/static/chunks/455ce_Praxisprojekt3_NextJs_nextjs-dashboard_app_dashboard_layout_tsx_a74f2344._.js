9b1a294c3ddd0028b3d9bed0ff8193fa22f49b17
mazouka1405 <mariamazouka@gmail.com>
2026-05-15 18:33:16 +0200

    Kapitel 5 abgeschlossen

--- .next/static/chunks/455ce_Praxisprojekt3_NextJs_nextjs-dashboard_app_dashboard_layout_tsx_a74f2344._.js
+++ .next/static/chunks/455ce_Praxisprojekt3_NextJs_nextjs-dashboard_app_dashboard_layout_tsx_a74f2344._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/4409a_next_dist_eda914fd._.js"
+  "static/chunks/Desktop_Master_lokal_S2_Frontend_Praxisprojekt3_NextJs_nextjs-dashboard_14d6ac43._.js"
 ],
     source: "dynamic"
 });
